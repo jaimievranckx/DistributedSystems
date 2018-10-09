d7e0d7dd0a3c520585feda5604f65108ab261a0c
omitoz <timo.nelen@gmail.com>
2018-10-09 09:21:52 +0200

    hej ho hej ho

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,6 +1,7 @@
 public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
+        //hej ho hej ho
         System.out.println("Hello, World");
     }
 }
