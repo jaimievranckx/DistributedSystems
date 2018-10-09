9ae2cf653a8d27482381b6c81a6f6ae76b04c667
omitoz <timo.nelen@gmail.com>
2018-10-09 10:16:49 +0200

    code cleanup

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -2,18 +2,7 @@ public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
 
-        //hej ho hej ho
+        System.out.println("Hello there :p ");
 
-        // TEST 1 2 3
-        System.out.println("Hello, World");
-
-        //hej ho hej ho test 2 lalala
-        //push via intellij lol
-        // merge testbranch
-        //ik ga dit naar testbracnh uploaden
-        //test 1000000000000000
-        System.out.println("Hello, people");
-
-        System.out.println("Testing Branches");
     }
 }
