bef27fc7c7bfb722e8c069e060ed26d799ed4743
gauthierdb <36762689+gauthierdb@users.noreply.github.com>
2018-10-09 09:30:17 +0200

    Update src/main/java/helloworld.java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -2,6 +2,8 @@ public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
         //hej ho hej ho
+
+        // TEST 1 2 3
         System.out.println("Hello, World");
     }
 }
