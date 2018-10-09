4f982d5c42224457a288d54ae3c54cc01a2d687e
gauthierdb <36762689+gauthierdb@users.noreply.github.com>
2018-10-09 09:32:26 +0200

    Update src/main/java/helloworld.java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,14 +1,15 @@
 public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
-<<<<<<< HEAD
+
         //hej ho hej ho
 
         // TEST 1 2 3
         System.out.println("Hello, World");
-=======
+
         //hej ho hej ho test 2 lalala
         System.out.println("Hello, people");
->>>>>>> 7a613ae4d34d6bdcb6d7be7ccb29db71d87c65c2
+
+        System.out.println("Testing Branch");
     }
 }
