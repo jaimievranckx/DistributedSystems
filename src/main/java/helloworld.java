634a017e0969472329fe461e08ae254671739e22
Jaimievranckx <jaimie_v@live.nl>
2018-10-21 14:14:51 +0200

    Update src/main/java/helloworld.java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,16 +1,9 @@
 public class helloworld {
     public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-<<<<<<< HEAD
-        //hej ho hej ho
-
-        // TEST 1 2 3
         System.out.println("Hello, World");
-=======
         //hej ho hej ho test 2 lalala
         //push via intellij lol
         // merge testbranch
         System.out.println("Hello, people");
->>>>>>> 7a613ae4d34d6bdcb6d7be7ccb29db71d87c65c2
     }
 }
